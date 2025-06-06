c16232cecddf15898028b4193a598e11b9962522
RobotSean <xueyangyang111@gmail.com>
2025-06-06 20:27:50 +0800

    完成boot加载loader并从汇编进入c代码

---
+++ build/source/loader/CMakeFiles/loader.dir/loader_16.c.obj.d
@@ -0,0 +1,3 @@
+source/loader/CMakeFiles/loader.dir/loader_16.c.obj: \
+ /home/xdu/cpp/Tinyx86os/source/loader/loader_16.c \
+ /usr/include/stdc-predef.h
---
+++ build/source/loader/CMakeFiles/loader.dir/loader_32.c.obj.d
@@ -0,0 +1,3 @@
+source/loader/CMakeFiles/loader.dir/loader_32.c.obj: \
+ /home/xdu/cpp/Tinyx86os/source/loader/loader_32.c \
+ /usr/include/stdc-predef.h
