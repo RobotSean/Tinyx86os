7279b89c036a0dd62870f503bbd6777403dbcc55
RobotSean <xueyangyang111@gmail.com>
2025-06-13 15:31:02 +0800

    添加list数据结构

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -10,6 +10,7 @@
 #include "tools/log.h"
 #include "tools/klib.h"
 #include "core/task.h"
+#include "tools/list.h"
 static boot_info_t * init_boot_info;        // 启动信息
 /**
  * 内核入口
@@ -21,6 +22,59 @@ void kernel_init (boot_info_t * boot_info) {
     log_init();
     time_init();
 }
+
+
+
+void list_test (void) {
+    list_t list;
+    list_node_t nodes[5];
+    
+    list_init(&list);
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    // 插入
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_first(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = list_remove_first(&list);
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+    // 插入
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+        list_remove(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    struct type_t {
+        int i;
+        list_node_t node;
+    }v = {0x123456};
+
+    list_node_t * v_node = &v.node;
+    struct type_t * p = list_node_parent(v_node, struct type_t, node);
+    if (p->i != 0x123456) {
+        log_printf("error");
+    }
+ }
+
 
 
 static task_t first_task;       // 第一个任务
@@ -54,6 +108,9 @@ void init_main(void) {
     // int a = 3;
     // ASSERT(a > 2);
     // ASSERT(a < 2);
+
+
+    list_test();
 
 
     task_init(&init_task, (uint32_t)init_task_entry, (uint32_t)&init_task_stack[1024]);
