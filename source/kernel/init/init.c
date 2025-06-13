da970e9f635e9bf8cc8354c5e3a798f82db09375
RobotSean <xueyangyang111@gmail.com>
2025-06-13 16:53:50 +0800

    任务切换：将当前任务删除并添加至队尾，队首作为新任务

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -21,63 +21,12 @@ void kernel_init (boot_info_t * boot_info) {
     cpu_init();
     log_init();
     time_init();
+
+    task_manager_init();
 }
 
 
 
-void list_test (void) {
-    list_t list;
-    list_node_t nodes[5];
-    
-    list_init(&list);
-    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
-
-    // 插入
-    for (int i = 0; i < 5; i++) {
-        list_node_t * node = nodes + i;
-        log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
-        list_insert_first(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
-
-
-    for (int i = 0; i < 5; i++) {
-        list_node_t * node = list_remove_first(&list);
-        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
-
-
-    // 插入
-    for (int i = 0; i < 5; i++) {
-        list_node_t * node = nodes + i;
-        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
-        list_insert_last(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
-
-    for (int i = 0; i < 5; i++) {
-        list_node_t * node = nodes + i;
-        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
-        list_remove(&list, node);
-    }
-    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
-
-    struct type_t {
-        int i;
-        list_node_t node;
-    }v = {0x123456};
-
-    list_node_t * v_node = &v.node;
-    struct type_t * p = list_node_parent(v_node, struct type_t, node);
-    if (p->i != 0x123456) {
-        log_printf("error");
-    }
- }
-
-
-
-static task_t first_task;       // 第一个任务
 static task_t init_task;
 static uint32_t init_task_stack[1024];	// 空闲任务堆栈
 
@@ -90,7 +39,6 @@ void init_task_entry(void) {
 
     for (;;) {
         log_printf("init task: %d", count++);
-        task_switch_from_to(&init_task, &first_task);
     }
 }
 
@@ -110,20 +58,17 @@ void init_main(void) {
     // ASSERT(a < 2);
 
 
-    list_test();
+    task_first_init();
+    task_init(&init_task, "init task", (uint32_t)init_task_entry, (uint32_t)&init_task_stack[1024]);
+    
+
+    irq_enable_global();
 
 
-    task_init(&init_task, (uint32_t)init_task_entry, (uint32_t)&init_task_stack[1024]);
-    //eip 和 esp 没有被设置到TSS段中，只为其设置了TSS段描述符，TSS段描述符指向TSS段
-    //当切换的时候硬件会自动保存TSS
-    task_init(&first_task, 0, 0);
-    //设置tr寄存器指向当前任务的 TSS段描述符
-    write_tr(first_task.tss_sel);
 
     int count = 0;
     for (;;) {
         log_printf("first task: %d", count++);
-        task_switch_from_to(&first_task, &init_task);
     }
 
 }
--- source/kernel/tools/list.c
+++ source/kernel/tools/list.c
@@ -119,3 +119,54 @@ list_node_t * list_remove(list_t *list, list_node_t *remove_node) {
     --list->count;
     return remove_node;
 }
+
+
+// void list_test (void) {
+//     list_t list;
+//     list_node_t nodes[5];
+    
+//     list_init(&list);
+//     log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+//     // 插入
+//     for (int i = 0; i < 5; i++) {
+//         list_node_t * node = nodes + i;
+//         log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
+//         list_insert_first(&list, node);
+//     }
+//     log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+//     for (int i = 0; i < 5; i++) {
+//         list_node_t * node = list_remove_first(&list);
+//         log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+//     }
+//     log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+//     // 插入
+//     for (int i = 0; i < 5; i++) {
+//         list_node_t * node = nodes + i;
+//         log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+//         list_insert_last(&list, node);
+//     }
+//     log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+//     for (int i = 0; i < 5; i++) {
+//         list_node_t * node = nodes + i;
+//         log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+//         list_remove(&list, node);
+//     }
+//     log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+//     struct type_t {
+//         int i;
+//         list_node_t node;
+//     }v = {0x123456};
+
+//     list_node_t * v_node = &v.node;
+//     struct type_t * p = list_node_parent(v_node, struct type_t, node);
+//     if (p->i != 0x123456) {
+//         log_printf("error");
+//     }
+//  }
