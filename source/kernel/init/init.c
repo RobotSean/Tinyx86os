f37e79daf7ce5a241d5045b589de4b73668dca69
RobotSean <xueyangyang111@gmail.com>
2025-06-16 19:52:30 +0800

    位图+地址分配结构

--- source/kernel/init/init.c
+++ source/kernel/init/init.c
@@ -20,6 +20,9 @@ void kernel_init (boot_info_t * boot_info) {
     init_boot_info = boot_info;
     // 初始化CPU:设置GDT、IDT
     cpu_init();
+    // 内存初始化要放前面一点，因为后面的代码可能需要内存分配
+    memory_init(boot_info);
+    
     log_init();
     time_init();
 
