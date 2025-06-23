22148da02da3f15cf7bc9fcbe887491e5f4e3b0a
RobotSean <xueyangyang111@gmail.com>
2025-06-23 17:04:57 +0800

    挂载设备文件系统

--- source/kernel/include/fs/fs.h
+++ source/kernel/include/fs/fs.h
@@ -5,7 +5,44 @@
 #define FILE_H
 
 #include <sys/stat.h>
+#include "file.h"
+#include "tools/list.h"
+#include "ipc/mutex.h"
 
+struct _fs_t;
+
+/**
+ * @brief 文件系统操作接口
+ */
+typedef struct _fs_op_t {
+	int (*mount) (struct _fs_t * fs,int major, int minor);
+    void (*unmount) (struct _fs_t * fs);
+    int (*open) (struct _fs_t * fs, const char * path, file_t * file);
+    int (*read) (char * buf, int size, file_t * file);
+    int (*write) (char * buf, int size, file_t * file);
+    void (*close) (file_t * file);
+    int (*seek) (file_t * file, uint32_t offset, int dir);
+    int (*stat)(file_t * file, struct stat *st);
+}fs_op_t;
+
+#define FS_MOUNTP_SIZE      512
+
+// 文件系统类型
+typedef enum _fs_type_t {
+    FS_DEVFS,
+}fs_type_t;
+
+typedef struct _fs_t {
+    char mount_point[FS_MOUNTP_SIZE];       // 挂载点路径长
+    fs_type_t type;              // 文件系统类型
+
+    fs_op_t * op;              // 文件系统操作接口
+    void * data;                // 文件系统的操作数据
+    int dev_id;                 // 所属的设备
+
+    list_node_t node;           // 下一结点
+    mutex_t * mutex;              // 文件系统操作互斥信号量
+}fs_t;
 void fs_init (void);
 
 int sys_open(const char *name, int flags, ...);
